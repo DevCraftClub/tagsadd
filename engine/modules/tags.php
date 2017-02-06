8adfc635b2a8b5d03f43b66d8b6a7ea3119587f9
MaHarder <info@maxim-harder.de>
2017-02-06 15:32:45 +0100

    Оптимизирован код
    Сделаны пару поправок

--- engine/modules/tags.php
+++ engine/modules/tags.php
@@ -2,41 +2,41 @@
 if (! defined ( 'DATALIFEENGINE' )) {
 	die ( "Hacking attempt!" );
 }
+require_once (ENGINE_DIR.'/api/api.class.php');
 include ENGINE_DIR . '/data/tagsadd.php';
 
 extract($_POST);
 
-if($tagsconf['onof'] == 1 && $tagsconf['send'] == 1) {
-	$subject = "Пользовательские теги к {$title}";
-	$subjecto = "Ваши теги к {$title}";
-	$mess = "{$username} предложил к новости <a href=\"{$link}\" target=\"_blank\" >{$title}</a> новые теги: {$tags}.";
-	$messo = "Вами предложенные теги к <a href=\"{$link}\" target=\"_blank\" >{$title}</a>: {$tags}.";
+if($tagsconf['onof'] && $tagsconf['send']) {
 	$date = time();
 	$tagsadd = explode( ',', $_POST['utags'] );
 	$tag = array();
 	foreach ($tagsadd as $key) {
 		$tag[] = "<span class=\"label label-danger\">{$key}</span>";
 	}
-	$tags = implode(',', $tag);
+	$tags = implode(' ', $tag);
+	$subject = "Пользовательские теги к {$title}";
+	$subjecto = "Ваши теги к {$title}";
+	$mess = "{$username} предложил к новости <a href=\"{$link}\" target=\"_blank\" >{$title}</a> новые теги: {$tags}.";
+	$messo = "Вами предложенные теги к <a href=\"{$link}\" target=\"_blank\" >{$title}</a>: {$tags}.";
+
 	if (empty($_POST['utags'])) {
 		$link = "/";
 	} else {
 		$link = $link;
 		$master = $tagsconf['master'];
 		$admin = $tagsconf['admin'];
-		$users = $db->query("SELECT * FROM " . PREFIX . "_users WHERE name='{$admin}'");
+		$users = $db->super_query("SELECT * FROM " . PREFIX . "_users WHERE name='{$admin}'");
 		$adminid = $users['user_id'];
 		$db->super_query("INSERT INTO " . PREFIX . "_post_tags (news_id, username, tags) VALUES ('{$news}','{$username}','{$tags}')");
-		$db->query( "INSERT INTO " . USERPREFIX . "_pm (subj, text, user, user_from, date, pm_read, folder, sendid) values ('{$subject}','{$mess}','1','{$username}','{$date}','0','inbox','0')");
-		if($tagsconf['user'] == 1) $db->query( "INSERT INTO " . USERPREFIX . "_pm (subj, text, user, user_from, date, pm_read, folder, sendid) values ('{$subjecto}','{$messo}','{$userid}','{$master}','{$date}','0','inbox','0')");
+		$db->query( "INSERT INTO " . USERPREFIX . "_pm (subj, text, user, user_from, date, pm_read, folder, sendid) values ('{$subject}','{$mess}','{$adminid}','{$username}','{$date}','0','inbox','0')");
+		if($tagsconf['user']) $db->query( "INSERT INTO " . USERPREFIX . "_pm (subj, text, user, user_from, date, pm_read, folder, sendid) values ('{$subjecto}','{$messo}','{$userid}','{$master}','{$date}','0','inbox','0')");
 		$db->query( "UPDATE " . USERPREFIX . "_users SET pm_all=pm_all+1, pm_unread=pm_unread+1  WHERE user_id='{$adminid}'" );
-		if($tagsconf['user'] == 1) $db->query( "UPDATE " . USERPREFIX . "_users SET pm_all=pm_all+1, pm_unread=pm_unread+1  WHERE user_id='{$userid}'" );
+		if($tagsconf['user']) $db->query( "UPDATE " . USERPREFIX . "_users SET pm_all=pm_all+1, pm_unread=pm_unread+1  WHERE user_id='{$userid}'" );
 	}
 }
 
-if($tagsconf['onof'] != 1) $link = "/";
-
-print "<script language='Javascript'><!--
-function reload() {location = \"$link\"}; setTimeout('reload()', 0);
-//--></script>";
+print "<script type=\"text/javascript\">
+    window.location.replace(\"$link\");
+</script>";
 ?>
